736f471644d8e2282bfe87cc1d43027302e704aa
stefano <panozzostefano@gmail.com>
2021-12-19 19:29:21 +0100

    Minor changes

--- hotelReservation/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua
+++ hotelReservation/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua
@@ -128,22 +128,26 @@ end
 
 -- PRINT TO CSV
 done = function(summary, latency, requests)
+  local rps = summary["requests"]/(summary["duration"]/1000000)
+  local eps = (summary["errors"]["connect"]+summary["errors"]["read"]+summary["errors"]["write"]+summary["errors"]["status"]+summary["errors"]["timeout"])/(summary["duration"]/1000000)
+  local rer = ((summary["errors"]["connect"]+summary["errors"]["read"]+summary["errors"]["write"]+summary["errors"]["status"]+summary["errors"]["timeout"])*100)/summary["requests"]
   -- open output file
-  f = io.open("result.csv", "a+")
-  f:write("time_started,min_latency,max_latency,mean_latency,stdev,50th,90th,99th,99.999th,duration,requests,bytes,connect_errors,read_errors,write_errors,status_errors,timeouts\n")
-  f:write(string.format("%s,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,%d,%d,%d\n",
-    os.date("!%Y-%m-%dT%TZ"),
-    latency.min,    -- minimum latency
-    latency.max,    -- max latency
-    latency.mean,   -- mean of latency
-    latency.stdev,  -- standard deviation of latency
+  f = io.open("result.csv", "w")
+  f:write("TS,COMPONENT,min-latency,max-latency,mean-latency,p50,p85,p90,p95,p99,duration,requests,bytes,connect-errors,read-errors,write-errors,status-errors,timeouts, rps, eps, rer\n")
+  f:write(string.format("%s,%s,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,%d,%d,%d,%d,%f,%f,%f\n",
+    os.date("!%Y-%m-%dT%T"),
+    "hotel_reservation",
+    latency.min/1000,    -- minimum latency
+    latency.max/1000,    -- max latency
+    latency.mean/1000,   -- mean of latency
 
-    latency:percentile(50),     -- 50percentile latency
-    latency:percentile(90),     -- 90percentile latency
-    latency:percentile(99),     -- 99percentile latency
-    latency:percentile(99.999), -- 99.999percentile latency
+    latency:percentile(50)/1000,     -- 50percentile latency
+    latency:percentile(85)/1000,     -- 85percentile latency
+    latency:percentile(90)/1000,     -- 90percentile latency
+    latency:percentile(95)/1000,     -- 99percentile latency
+    latency:percentile(99)/1000,     -- 99.999percentile latency
       
-    summary["duration"],          -- duration of the benchmark
+    summary["duration"]/1000000,  -- duration of the benchmark in seconds
     summary["requests"],          -- total requests during the benchmark
     summary["bytes"],             -- total received bytes during the benchmark
      
@@ -151,7 +155,11 @@ done = function(summary, latency, requests)
     summary["errors"]["read"],    -- total socket read errors
     summary["errors"]["write"],   -- total socket write errors
     summary["errors"]["status"],  -- total socket write errors
-    summary["errors"]["timeout"]  -- total request timeouts
+    summary["errors"]["timeout"], -- total request timeouts
+                        
+    rps,
+    eps,
+    rer    
     ))
   
   f:close()
